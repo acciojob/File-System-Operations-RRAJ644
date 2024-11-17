111933d468320845b3573a45f6a857e3d020907d
RRAJ644 <riturajsingh.17248@gmail.com>
2024-11-17 17:13:00 +0530

    task

--- main.js
+++ main.js
@@ -12,17 +12,13 @@ fs.readFile(jsonFilePath, 'utf8', (err, data) => {
 
   // TODO: Perform the required operations on the users data
 
-  // Print the total number of users
   console.log(`Total number of users: ${users.length}`);
 
-  // Find the user with the highest score and print their details
   const highestScoreUser = users.reduce((prev, current) => (prev.score > current.score ? prev : current));
   console.log('User with the highest score:', highestScoreUser);
 
-  // Sort the users based on their scores in descending order
   users.sort((a, b) => b.score - a.score);
 
-  // Write the sorted data back to the JSON file
   fs.writeFile(jsonFilePath, JSON.stringify(users, null, 2), (err) => {
     if (err) {
       console.error(`Error writing file: ${err}`);
